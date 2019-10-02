737bb3eac2d863fbed63d7ea05716ad204d0391c
Zowie van Geest <zowie@zowievangeest.nl>
2019-10-02 15:11:21 +0200

    fix: center menu item

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -12,7 +12,7 @@
     <div class="block lg:hidden">
       <button
         @click="toggle"
-        class="flex items-center px-3 py-2 border rounded text-voorhoede-blue border-voorhoede-blue hover:text-white hover:border-white"
+        class="flex items-center px-3 py-2 border rounded text-voorhoede-blue border-voorhoede-blue"
       >
         <svg
           class="fill-current h-3 w-3"
@@ -28,10 +28,10 @@
       :class="open ? 'block' : 'hidden'"
       class="w-full block flex-grow lg:flex lg:items-center lg:w-auto"
     >
-      <div class="text-lg lg:flex-grow text-voorhoede-blue">
+      <div class="text-lg text-center lg:flex-grow text-voorhoede-blue">
         <router-link
           to="/blog"
-          class="block mt-4 lg:inline-block lg:mt-0 text-voorhoede-blue hover:text-white mr-4"
+          class="block mt-4 lg:inline-block lg:mt-0 text-voorhoede-bluemr-4"
           >Blog posts</router-link
         >
       </div>
